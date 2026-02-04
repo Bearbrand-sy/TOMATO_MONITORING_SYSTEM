0df1ae1e79d4fc93e0949d500bcbffa16183d7ad
bearbrand-sy <sanderperejan@gmail.com>
2026-02-04 20:35:09 +0800

    added code

--- dashboard.js
+++ dashboard.js
@@ -9,6 +9,30 @@ function showSection(sectionId) {
             event.target.classList.add('active');
         }
 
+const transportData = {
+    1: { temp: "26°C", hum: "52%", vent: "ON", route: "Manolo Farm → CDO Market", status: "Safe" },
+    2: { temp: "30°C", hum: "60%", vent: "OFF", route: "Manolo Farm → Davao Market", status: "Warning" },
+    3: { temp: "22°C", hum: "48%", vent: "ON", route: "Manolo Farm → General Santos Market", status: "Safe" }
+};
+
+function updateTransport() {
+    const id = document.getElementById("transpo").value;
+    if (!id) return;
+
+    const t = transportData[id];
+
+    document.getElementById("temperature").textContent = t.temp;
+    document.getElementById("humidity").textContent = t.hum;
+    document.getElementById("ventilation").textContent = t.vent;
+
+    
+    document.getElementById("d-id").textContent = "Transport " + id;
+    document.getElementById("d-route").textContent = t.route;
+    document.getElementById("d-status").textContent = t.status;
+
+    document.getElementById("details").classList.remove("hidden");
+}
+
 function logout() {
             if (confirm('Are you sure you want to logout?')) {
                  window.location.href="index.html"; 
