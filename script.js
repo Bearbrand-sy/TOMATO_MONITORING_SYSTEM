9af4fa4f76aa4765a67b7a93e376fc6487dfd5e4
bearbrand-sy <sanderperejan@gmail.com>
2026-02-04 19:18:56 +0800

    added script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+const validUserEmail = "admin";
+const validUserPass = "12345";
+
+function login() {
+
+    const userEmail = document.getElementById("Email").value;
+    const userPass = document.getElementById("Password").value;
+
+    if (userEmail === validUserEmail && userPass === validUserPass) {
+        alert("Login Successful");
+        window.location.href = "dashboard.html";
+    } else {
+        alert("Invalid username or password");
+    }
+}
