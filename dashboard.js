aca024e8774e0c98396be1d75d2fc068f21599bd
bearbrand-sy <sanderperejan@gmail.com>
2026-02-04 19:29:44 +0800

    added function for section

--- dashboard.js
+++ dashboard.js
@@ -1,3 +1,14 @@
+function showSection(sectionId) {
+        document.querySelectorAll('.main > .section').forEach(section => {
+            section.classList.add('hidden');
+        });
+            document.getElementById(sectionId).classList.remove('hidden');
+            document.querySelectorAll('.sidebar button').forEach(btn => {
+                btn.classList.remove('active');
+            });
+            event.target.classList.add('active');
+        }
+
 function logout() {
             if (confirm('Are you sure you want to logout?')) {
                  window.location.href="index.html"; 
